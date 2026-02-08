33ed3998741ea313dbd7020448687ef49d6c1b29
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2026-02-08 11:19:40 +0100

    Add new page components for NotFound, Contacts, Home, and Portfolio
    - Created NotFoundPage component
    - Created ContactsPage component
    - Created HomePage component
    - Created PortfolioPage component

--- Busines-LandingPage/src/App.tsx
+++ Busines-LandingPage/src/App.tsx
@@ -1,5 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./App.scss";
+import { HomePage } from "./pages/homePage/HomePage";
+import { ContactsPage } from "./pages/contactsPage/ContactsPage";
+import { PortfolioPage } from "./pages/portfolioPage/PortfolioPage";
+import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/portfolio">Portfolio</NavLink>
+        <NavLink to="/contacts">Contacts</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 };
